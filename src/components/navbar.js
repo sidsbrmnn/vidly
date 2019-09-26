f61ae8bf082bd64a62de7e941914a682f54b9194
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-09-26 07:48:53 +0530

    toggle navbar for mobile devices

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,55 +1,71 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { cx } from '../utils/cx';
 
-const Navbar = ({ links }) => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link to="/" className="navbar-brand">
-          Vidly
-        </Link>
+class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
 
-        <button
-          type="button"
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  toggleNavbar = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
 
-        <div id="navbarSupportedContent" className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            {links.map(link => (
-              <li key={link.path} className="nav-item">
-                <NavLink to={link.path} className="nav-link">
-                  {link.label}
+  render() {
+    const { links } = this.props;
+    const { isOpen } = this.state;
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            Vidly
+          </Link>
+
+          <button
+            type="button"
+            className="navbar-toggler"
+            onClick={this.toggleNavbar}
+            aria-controls="navbarSupportedContent"
+            aria-expanded={isOpen}
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            id="navbarSupportedContent"
+            className={cx('navbar-collapse collapse', isOpen && 'show')}
+          >
+            <ul className="navbar-nav mr-auto">
+              {links.map(link => (
+                <li key={link.path} className="nav-item">
+                  <NavLink to={link.path} className="nav-link">
+                    {link.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
                 </NavLink>
               </li>
-            ))}
-          </ul>
-
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <NavLink to="/login" className="nav-link">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/register" className="nav-link">
-                Register
-              </NavLink>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Register
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 Navbar.defaultProps = {
   links: [],
