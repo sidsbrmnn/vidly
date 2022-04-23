7a308e3471607ad8bcd3d8926a1b5763b413c005
Siddharth Subramanian <sidsbrmnn@gmail.com>
2022-04-24 01:55:03 +0530

    use REACT_APP_API_BASE_URL for axios baseURL

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 http.interceptors.request.use(config => {
