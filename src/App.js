fc65e5210e321eeb1519c1ad19aaec42dbd2e93f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2022-04-24 11:24:49 +0530

    rewrite App as functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Component } from 'react';
 import {
   Redirect,
   Route,
   BrowserRouter as Router,
   Switch,
 } from 'react-router-dom';
+import './App.css';
 import { AuthProvider } from './components/common/auth';
 import ProtectedRoute from './components/common/protectedRoute';
 import Customers from './components/customers';
@@ -16,42 +16,39 @@ import Navbar from './components/navbar';
 import NotFound from './components/notFound';
 import Register from './components/register';
 import Rentals from './components/rentals';
-import './App.css';
 
-class App extends Component {
-  navLinks = [
+const App = () => {
+  const navLinks = [
     { path: '/movies', label: 'Movies' },
     { path: '/customers', label: 'Customers' },
     { path: '/rentals', label: 'Rentals' },
   ];
 
-  render() {
-    return (
-      <AuthProvider>
-        <Router>
-          <header>
-            <Navbar links={this.navLinks} />
-          </header>
-          <main className="container">
-            <Switch>
-              <ProtectedRoute path="/movies/:id" component={MovieForm} />
-              <Route path="/movies" component={Movies} />
-              <ProtectedRoute path="/customers" component={Customers} />
-              <ProtectedRoute path="/rentals" component={Rentals} />
-              <Route path="/not-found" component={NotFound} />
+  return (
+    <AuthProvider>
+      <Router>
+        <header>
+          <Navbar links={navLinks} />
+        </header>
+        <main className="container">
+          <Switch>
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <ProtectedRoute path="/customers" component={Customers} />
+            <ProtectedRoute path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
 
-              <Route path="/login" component={Login} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/register" component={Register} />
 
-              <Redirect from="/" exact to="/movies" />
-              <Redirect to="/not-found" />
-            </Switch>
-          </main>
-        </Router>
-      </AuthProvider>
-    );
-  }
-}
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default App;
