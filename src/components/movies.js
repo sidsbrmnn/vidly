a9237005f077019d1db661294bd2b67cdecf6750
Siddharth Subramanian <sidsbrmnn@gmail.com>
2022-04-24 11:23:47 +0530

    rewrite Movies component as functional with hooks and useAuth

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,10 +1,10 @@
 import orderBy from 'lodash/orderBy';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getGenres } from '../services/genreService';
 import { deleteMovie, getMovies } from '../services/movieService';
 import paginate from '../utils/paginate';
-import { withAuth } from './common/auth';
+import { useAuth } from './common/auth';
 import Like from './common/like';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
@@ -16,18 +16,27 @@ const ALL_GENRES = {
   name: 'All Genres',
 };
 
-class Movies extends Component {
-  state = {
-    movies: [],
-    genres: [],
-    selectedGenre: null,
-    pageSize: 4,
-    currentPage: 1,
-    sortColumn: { path: 'title', order: 'asc' },
-    searchQuery: '',
-  };
+const Movies = () => {
+  const auth = useAuth();
 
-  columns = [
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [pageSize] = useState(4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortColumn, setSortColumn] = useState({ path: 'title', order: 'asc' });
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    getMovies().then(({ data }) => setMovies(data));
+    getGenres().then(({ data }) => {
+      data.unshift(ALL_GENRES);
+      setGenres(data);
+      setSelectedGenre(ALL_GENRES);
+    });
+  }, []);
+
+  const columns = [
     {
       path: 'title',
       label: 'Title',
@@ -39,17 +48,16 @@ class Movies extends Component {
     {
       key: 'like',
       content: movie => (
-        <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+        <Like liked={movie.liked} onClick={() => handleLike(movie)} />
       ),
     },
     {
       key: 'delete',
       content: movie => {
-        const { auth } = this.props;
         if (auth.payload && auth.payload.isAdmin) {
           return (
             <button
-              onClick={() => this.handleDelete(movie)}
+              onClick={() => handleDelete(movie)}
               className="btn btn-danger btn-sm"
             >
               Delete
@@ -62,54 +70,38 @@ class Movies extends Component {
     },
   ];
 
-  async componentDidMount() {
-    const { data: movies } = await getMovies();
-    const { data: genres } = await getGenres();
-    genres.unshift(ALL_GENRES);
-    this.setState({ movies, genres, selectedGenre: ALL_GENRES });
-  }
-
-  handleDelete = async movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies });
+  const handleDelete = async movie => {
+    setMovies(movies.filter(m => m._id !== movie._id));
 
     await deleteMovie(movie._id);
   };
 
-  handleLike = movie => {
-    const movies = [...this.state.movies];
-    const index = movies.indexOf(movie);
-    movies[index] = { ...movies[index] };
-    movies[index].liked = !movies[index].liked;
-    this.setState({ movies });
+  const handleLike = movie => {
+    const updatedMovies = movies.map(m =>
+      m._id === movie._id ? { ...m, liked: !m.liked } : m
+    );
+    setMovies(updatedMovies);
   };
 
-  handlePageChange = page => {
-    this.setState({ currentPage: page });
+  const handlePageChange = page => {
+    setCurrentPage(page);
   };
 
-  handleGenreSelect = genre => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+  const handleGenreSelect = genre => {
+    setSelectedGenre(genre);
+    setCurrentPage(1);
   };
 
-  handleSort = sortColumn => {
-    this.setState({ sortColumn });
+  const handleSort = sortColumn => {
+    setSortColumn(sortColumn);
   };
 
-  handleSearch = searchQuery => {
-    this.setState({ searchQuery, currentPage: 1 });
+  const handleSearch = searchQuery => {
+    setSearchQuery(searchQuery);
+    setCurrentPage(1);
   };
 
-  getPagedMovies = () => {
-    const {
-      movies,
-      selectedGenre,
-      currentPage,
-      pageSize,
-      sortColumn,
-      searchQuery,
-    } = this.state;
-
+  const getPagedMovies = () => {
     let filtered =
       selectedGenre && selectedGenre._id !== ALL_GENRES._id
         ? movies.filter(m => m.genre._id === selectedGenre._id)
@@ -125,67 +117,54 @@ class Movies extends Component {
     return { pagedMovies, filteredCount: filtered.length };
   };
 
-  render() {
-    const { auth } = this.props;
-    const {
-      movies,
-      genres,
-      selectedGenre,
-      pageSize,
-      currentPage,
-      sortColumn,
-      searchQuery,
-    } = this.state;
-
-    if (movies.length === 0) {
-      return (
-        <section className="py-5">
-          <p>There are no movies in the database.</p>
-        </section>
-      );
-    }
-
-    const { pagedMovies, filteredCount } = this.getPagedMovies();
-
+  if (movies.length === 0) {
     return (
       <section className="py-5">
-        <div className="row">
-          <div className="col-12 col-md-3">
-            <ListGroup
-              items={genres}
-              selectedItem={selectedGenre}
-              onItemSelect={this.handleGenreSelect}
-            />
-          </div>
-          <div className="col-12 col-md-9 mt-4 mt-md-0 space-y-3">
-            <SearchBox value={searchQuery} onChange={this.handleSearch} />
-            <div className="d-sm-flex flex-col-reverse justify-content-between align-items-center space-y-3 space-y-sm-0">
-              <div>Showing {filteredCount} movies in the database.</div>
-              {auth.payload && auth.payload.isAdmin && (
-                <Link to="/movies/new" className="btn btn-primary">
-                  New Movie
-                </Link>
-              )}
-            </div>
-            <div>
-              <Table
-                columns={this.columns}
-                sortColumn={sortColumn}
-                data={pagedMovies}
-                onSort={this.handleSort}
-              />
-              <Pagination
-                itemsCount={filteredCount}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageChange={this.handlePageChange}
-              />
-            </div>
-          </div>
-        </div>
+        <p>There are no movies in the database.</p>
       </section>
     );
   }
-}
 
-export default withAuth(Movies);
+  const { pagedMovies, filteredCount } = getPagedMovies();
+
+  return (
+    <section className="py-5">
+      <div className="row">
+        <div className="col-12 col-md-3">
+          <ListGroup
+            items={genres}
+            selectedItem={selectedGenre}
+            onItemSelect={handleGenreSelect}
+          />
+        </div>
+        <div className="col-12 col-md-9 mt-4 mt-md-0 space-y-3">
+          <SearchBox value={searchQuery} onChange={handleSearch} />
+          <div className="d-sm-flex flex-col-reverse justify-content-between align-items-center space-y-3 space-y-sm-0">
+            <div>Showing {filteredCount} movies in the database.</div>
+            {auth.payload && auth.payload.isAdmin && (
+              <Link to="/movies/new" className="btn btn-primary">
+                New Movie
+              </Link>
+            )}
+          </div>
+          <div>
+            <Table
+              columns={columns}
+              sortColumn={sortColumn}
+              data={pagedMovies}
+              onSort={handleSort}
+            />
+            <Pagination
+              itemsCount={filteredCount}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Movies;
